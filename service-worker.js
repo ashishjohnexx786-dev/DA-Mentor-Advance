07ef5d81991e54e9512ead8c3c32a050b8c5bea8
Ashish Kumar <ashishjohnexx786@gmail.com>
2026-08-24 03:51:29 +0530

    Refresh Advance PWA cache for v2.1 fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="da-mentor-advance-v2.0";
+const CACHE="da-mentor-advance-v2.1";
 const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./curriculum.js","./app.js","./hotfix.js","./enhancements.js","./theme-picker.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&(k.startsWith("da-mentor-pro")||k.startsWith("da-mentor-advance"))).map(k=>caches.delete(k)))));self.clients.claim();});
