f8beaba0b05e1bafe58aa6fe4e2b2c458089602c
Ashish Kumar <ashishjohnexx786@gmail.com>
2026-08-23 21:52:01 +0530

    Refresh offline cache for Mentor Pro v1.1

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="da-mentor-pro-v1.0.1";
-const ASSETS=["./","./index.html","./styles.css","./curriculum.js","./app.js","./hotfix.js","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{if(e.request.method==="GET"){const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return resp}).catch(()=>caches.match("./index.html")))));
+const CACHE="da-mentor-pro-v1.1";
+const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./curriculum.js","./app.js","./hotfix.js","./enhancements.js","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
