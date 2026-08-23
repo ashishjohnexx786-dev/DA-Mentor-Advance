3672f4914770eba34421a0b455060a19dbaebcff
Ashish Kumar <ashishjohnexx786@gmail.com>
2026-08-23 21:43:02 +0530

    Cache Mentor Pro v1.0.1 hotfix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="da-mentor-pro-v1";
-const ASSETS=["./","./index.html","./styles.css","./curriculum.js","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="da-mentor-pro-v1.0.1";
+const ASSETS=["./","./index.html","./styles.css","./curriculum.js","./app.js","./hotfix.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{if(e.request.method==="GET"){const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return resp}).catch(()=>caches.match("./index.html")))));
