2d9d02477553912ac6f733d59591803add30e759
Ashish Kumar <ashishjohnexx786@gmail.com>
2026-08-23 22:16:45 +0530

    Cache direct theme picker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="da-mentor-pro-v1.3";
-const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./curriculum.js","./app.js","./hotfix.js","./enhancements.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="da-mentor-pro-v1.4";
+const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./curriculum.js","./app.js","./hotfix.js","./enhancements.js","./theme-picker.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
