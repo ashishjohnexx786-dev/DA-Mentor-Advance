d2bd8229c400f09695c12a92bb0a6c4fb1e16b37
Ashish Kumar <ashishjohnexx786@gmail.com>
2026-08-23 22:02:58 +0530

    Refresh Mentor Pro offline cache for v1.2 toolkit

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="da-mentor-pro-v1.2";
+const CACHE="da-mentor-pro-v1.2.1";
 const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./curriculum.js","./app.js","./hotfix.js","./enhancements.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
